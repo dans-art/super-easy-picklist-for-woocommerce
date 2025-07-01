52f34a5f357df1709abaf307a4fbd61ae9798500
Dan's Art <spy15@bluewin.ch>
2025-07-01 19:30:28 +0300

    Updated DaTemplateHandler

--- super-easy-picklist-for-woocommerce.php
+++ super-easy-picklist-for-woocommerce.php
@@ -7,8 +7,8 @@
  * Contributors: dansart
  * Contributors URL: http://dev.dans-art.ch
  * Tags: woocommerce, customer, tools, helper
- * Version: 0.1
- * Stable tag: 0.1
+ * Version: 0.2
+ * Stable tag: 0.2
  * 
  * Requires at least: 5.4.0
  * Tested up to: 5.9
